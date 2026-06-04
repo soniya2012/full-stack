eac8f98302ee1194aeafca154ce31276a27d92b7
soniya <soniya20122007@gmail.com>
2026-06-04 15:41:56 +0530

    javascript is variables,datatype and variables completed

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+var a =10; //Reinitialize &Redeclare
+let b = 20; //Reintialize
+const c = 30;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var num=10;
+var str ="soni";
+var undefine;
+var Null = null;
+var boolean = true;
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof undefine);
+console.log(typeof Null);
+
+
+
+var bignit = 123n;
+var symbol = symbol('li')
+
+console.log(typeof bignit)
+console.log(typeof symbol)
+
